0e7fbb4816e96643292f536a520613d715256e9d
baba-s <shotababa1206@gmail.com>
2022-09-25 14:41:24 +0900

    fix

--- Editor/AudioPreprocessorSettings.cs
+++ Editor/AudioPreprocessorSettings.cs
@@ -1,31 +1,10 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿using UnityEditor;
 
 namespace Kogane.Internal
 {
     [FilePath( "ProjectSettings/Kogane/AudioPreprocessorSettings.asset", FilePathAttribute.Location.ProjectFolder )]
     internal sealed class AudioPreprocessorSettings :
-        ScriptableSingleton<AudioPreprocessorSettings>,
-        IEnumerable<AudioPreprocessorSetting>
+        PreprocessorSettingsBase<AudioPreprocessorSettings, AudioPreprocessorSetting>
     {
-        [SerializeField] private AudioPreprocessorSetting[] m_array = Array.Empty<AudioPreprocessorSetting>();
-
-        public void Save()
-        {
-            Save( true );
-        }
-
-        public IEnumerator<AudioPreprocessorSetting> GetEnumerator()
-        {
-            return ( ( IEnumerable<AudioPreprocessorSetting> )m_array ).GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
     }
 }
